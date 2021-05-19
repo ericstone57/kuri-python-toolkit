4ab57aa4a6a7d7388e2fbfec4ba270e09809300e
Eric Lee <ericstone.dev@gmail.com>
2021-05-19 22:55:21 +0800

    init the project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kuri-python-toolkit',
+    version='0.0.1',
+    description='Toolkit package for KURI Service',
+    url='git@github.com:ericstone57/kuri-python-toolkit.git',
+    author='Eric Lee',
+    author_email='ericstone.dev@gmail.com',
+    license='unlicense',
+    packages=setuptools.find_packages(),
+)
