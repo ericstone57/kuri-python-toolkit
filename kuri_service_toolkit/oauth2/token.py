236619e97f8e79647273176da643adcba90131a0
Eric Lee <ericstone.dev@gmail.com>
2021-05-20 12:52:35 +0800

    update token validate func

--- kuri_service_toolkit/oauth2/token.py
+++ kuri_service_toolkit/oauth2/token.py
@@ -1,2 +1,25 @@
-def token_validate():
-    print('valid!')
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, ExpiredSignatureError, JWTError
+from jose.exceptions import JWTClaimsError
+import os
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+
+def token_validate(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(
+            token,
+            os.getenv('OAUTH_PUBLIC_KEY'),
+            algorithms=['ES256'],
+            audience=os.getenv('UC_AUDIENCE'),
+            issuer=f'{os.getenv("UC_DOMAIN")}/token/{os.getenv("UC_APPID")}/'
+        )
+        if not ('cid' in payload and payload['cid'] == os.getenv('UC_CLIENT_ID')):
+            raise HTTPException(
+                status_code=401, detail='application id invalid.')
+    except (JWTError, ExpiredSignatureError, JWTClaimsError) as err:
+        raise HTTPException(status_code=401, detail=str(err))
+    else:
+        return True
